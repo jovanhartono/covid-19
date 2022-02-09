3fb2de628b9b27ddd803ee373ca2cf9458a0edcf
jovan <jovanus@yahoo.co.id>
2022-02-09 23:32:39 +0700

    Fixing "" value on object key. Adding optional slug attribute on Card interface

--- interfaces/vaccine.ts
+++ interfaces/vaccine.ts
@@ -60,18 +60,18 @@ interface Vaccine {
         {
             key_as_string: string,
             key: number,
-            doc_count: 1,
+            doc_count: number,
             jumlah_vaksinasi_2: {
-                value: 0
+                value: number
             },
             jumlah_vaksinasi_1: {
-                value: 0
+                value: number
             },
             jumlah_jumlah_vaksinasi_1_kum: {
-                value: 0
+                value: number
             },
             jumlah_jumlah_vaksinasi_2_kum: {
-                value: 0
+                value: number
             }
         }
     ],
