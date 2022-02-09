c2e3513171745c7e4a96fdb7a421f3fe7507862a
Windows-Jovan <jovanus@yahoo.co.id>
2022-02-09 14:51:31 +0700

    Adding details page, and Wrapping the card using Link from next.js

--- components/card.tsx
+++ components/card.tsx
@@ -1,5 +1,6 @@
 import {CardSection, Card} from "../interfaces/general";
 import * as React from "react";
+import Link from "next/link";
 
 interface CardProps {
     section: CardSection
@@ -10,12 +11,14 @@ function Cards({section}: CardProps) {
         <div className="not-prose w-full grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-3 md:gap-5 xl:gap-7 mt-3">
             {section.datas.map((card: Card, index: number) => {
                 return (
-                    <div key={index} className="rounded-lg p-3 transition-all duration-200 ease-in-out shadow-lg
+                    <Link key={index} href={`/details/${index}`}>
+                        <a className="rounded-lg p-3 transition-all duration-200 ease-in-out shadow-lg
                     hover:shadow-xl hover:-translate-y-0.5">
-                        <card.icon className={`w-6 h-6 ${card.iconColor}`}/>
-                        <span className="text-xl text-gray-900 font-semibold">{card.value}</span>
-                        <p className="font-light text-sm">{card.title}</p>
-                    </div>
+                            <card.icon className={`w-6 h-6 ${card.iconColor}`}/>
+                            <span className="text-xl text-gray-900 font-semibold">{card.value}</span>
+                            <p className="font-light text-sm">{card.title}</p>
+                        </a>
+                    </Link>
                 )
             })}
         </div>
