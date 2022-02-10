93cec6be08d5a139acf2fd5a8cda01c84decd941
jovan <jovanus@yahoo.co.id>
2022-02-10 15:15:48 +0700

    Creating Details page

--- components/card.tsx
+++ components/card.tsx
@@ -11,7 +11,7 @@ function Cards({section}: CardProps) {
         <div className="not-prose w-full grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-3 md:gap-5 xl:gap-7 mt-3">
             {section.datas.map((card: Card, index: number) => {
                 return (
-                    <Link key={index} href={`/details/${index}`}>
+                    <Link key={index} href={`/details`}>
                         <a className="rounded-lg p-3 transition-all duration-200 ease-in-out shadow-lg
                     hover:shadow-xl hover:-translate-y-0.5">
                             <card.icon className={`w-6 h-6 ${card.iconColor}`}/>
--- pages/details/index.tsx
+++ pages/details/index.tsx
@@ -1,11 +1,122 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {GetStaticProps, NextPage} from "next";
+import {AxiosResponse} from "axios";
+import {Case, DailyCase} from "../../interfaces/general";
+import axios from "../../config/axios";
+import {VaccineSpecimen} from "../../interfaces/vaccine";
+import {CartesianGrid, Line, LineChart, ResponsiveContainer, Tooltip, TooltipProps, XAxis, YAxis} from "recharts";
+import dayjs from "dayjs";
+import {NameType, ValueType} from "recharts/types/component/DefaultTooltipContent";
 
-function Details() {
+interface DetailsProps {
+    cases: Case;
+    vaccine: VaccineSpecimen;
+}
+
+interface ButtonTime {
+    value: string,
+    label: string,
+    dayValue: number
+}
+
+const buttons: ButtonTime[] = [
+    {
+        value: 'week',
+        label: 'One Week',
+        dayValue: Date.now() - (8 * 24 * 60 * 60 * 1000)
+    },
+    {
+        value: 'month',
+        label: 'One Month',
+        dayValue: Date.now() - (30 * 24 * 60 * 60 * 1000)
+    },
+    {
+        value: 'year',
+        label: 'One Year',
+        dayValue: Date.now() - (365 * 24 * 60 * 60 * 1000)
+    },
+    {
+        value: 'forever',
+        label: 'Since 🔥 Nation Attack',
+        dayValue: 0
+    }
+]
+
+const Details: NextPage<DetailsProps> = ({cases, vaccine}: DetailsProps) => {
+    const dailyCases: DailyCase[] = cases.update.harian;
+    const [data, setData] = useState(cases.update.harian);
+    const [active, setActive] = useState({active: 'forever'});
+
     return (
-        <div>
-            <h1>Details Page</h1>
-        </div>
+        <>
+            <h1 className='text-transparent text-center bg-gradient-to-br from-pink-500 to-purple-500 bg-clip-text'>
+                Details Page</h1>
+            <div className="flex space-x-3 justify-end mb-6">
+                {buttons.map((button: ButtonTime, index: number) => {
+                    return (
+                        <button key={index}
+                                className={`${active.active === button.value ? 'border-none bg-gradient-to-r from-pink-500 to to-fuchsia-500 text-white' : ''} 
+                                hover:shadow-md text-xs border border-gray-300 font-light text-gray-900 rounded-full shadow-sm p-3`}
+                                onClick={() => setData(() => {
+                                    setActive({
+                                        active: button.value
+                                    })
+                                    return dailyCases.filter(x => x.key > button.dayValue)
+                                })}>
+                            {button.label}
+                        </button>
+                    );
+                })
+                }
+            </div>
+            <div className={'w-full prose-sm'}>
+                <h2>Daily Mortality</h2>
+                <ResponsiveContainer width="100%" aspect={2}>
+                    <LineChart width={300} height={100} data={data}>
+                        <XAxis
+                            dataKey="key"
+                            axisLine={false}
+                            tickLine={false}
+                            tickFormatter={(date: number) => dayjs(date).format('D/M/YYYY')}
+                        />
+                        <YAxis
+                            width={40}
+                            tickCount={7}
+                            axisLine={false}
+                            tickLine={false}
+                            tickFormatter={(value: number) => value.toLocaleString('id-ID')}
+                        />
+                        <Tooltip content={<CustomTooltip/>}/>
+                        <CartesianGrid vertical={false} opacity={0.3}/>
+                        <Line type="monotone" dataKey="jumlah_meninggal.value" stroke="#d946ef" strokeWidth={2}
+                              dot={false}/>
+                    </LineChart>
+                </ResponsiveContainer>
+            </div>
+        </>
     );
+
+    function CustomTooltip({active, payload, label}: TooltipProps<ValueType, NameType>) {
+        if (active) {
+            return <div className="shadow-lg bg-white p-3 rounded-lg">
+                <h5>{dayjs(label).format('dddd, DD MMM YYYY')}</h5>
+                <h5 className="text-fuchsia-600">{payload ? payload[0].value : null}</h5>
+            </div>
+        }
+        return null;
+    }
 }
 
 export default Details;
+
+export const getStaticProps: GetStaticProps = async () => {
+    const [cases, vaccine]: [AxiosResponse<Case>, AxiosResponse<VaccineSpecimen>] = await Promise.all([
+        axios.get('update.json').then(response => response.data),
+        axios.get('pemeriksaan-vaksinasi.json').then(response => response.data)
+    ]);
+
+    return {
+        props: {cases, vaccine},
+        revalidate: 21600
+    }
+}
