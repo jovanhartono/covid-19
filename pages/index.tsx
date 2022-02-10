17ea891643b37ac44830eaa0e4cc3ab2904f946d
jovan <jovanus@yahoo.co.id>
2022-02-10 15:14:48 +0700

    Extracting the parent closing tag with the layout.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,25 +14,22 @@ interface IndexProps {
 
 const Home: NextPage<IndexProps> = ({cases, vaccine}: IndexProps) => {
     return (
-        <div
-            className="flex justify-center prose prose-gray prose-lg sm:prose-xl xl:prose-2xl bg-gray-50 max-w-none prose-h3:mb-0 min-h-screen">
+        <>
             <Head>
                 <title>Covid-19 Info</title>
-                <meta name="description" content="General Information About Novel Corona Virus 19 in Indonesia." />
+                <meta name="description" content="General Information About Novel Corona Virus 19 in Indonesia."/>
             </Head>
-            <div className="p-5 w-full md:w-2/3 xl:w-1/2">
-                <h1 className="text-transparent text-center bg-gradient-to-br from-pink-500 to-purple-500 bg-clip-text">Indonesia
-                    Covid-19 Info</h1>
-                <Infection cases={cases} vaccine={vaccine}/>
-            </div>
-        </div>
+            <h1 className="text-transparent text-center bg-gradient-to-br from-pink-500 to-purple-500 bg-clip-text">Indonesia
+                Covid-19 Info</h1>
+            <Infection cases={cases} vaccine={vaccine}/>
+        </>
     )
 }
 
 export const getStaticProps: GetStaticProps = async () => {
     const [cases, vaccine]: [AxiosResponse<Case>, AxiosResponse<VaccineSpecimen>] = await Promise.all([
-        axios.get('update.json').then(response=> response.data),
-        axios.get('pemeriksaan-vaksinasi.json').then(response=> response.data)
+        axios.get('update.json').then(response => response.data),
+        axios.get('pemeriksaan-vaksinasi.json').then(response => response.data)
     ]);
 
     return {
