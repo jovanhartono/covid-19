379def7b7d51403ae675f6306cfadc00f391a176
jovan <jovanus@yahoo.co.id>
2022-02-11 19:09:55 +0700

    Changing layout width on xl-breakpoint

--- components/layout.tsx
+++ components/layout.tsx
@@ -7,7 +7,7 @@ interface LayoutProps {
 function Layout({children}: LayoutProps) {
     return (
         <div className={'prose prose-gray prose-lg sm:prose-xl xl:prose-2xl flex justify-center bg-gray-50 max-w-none min-h-screen'}>
-            <main className={'p-5 w-full md:w-2/3 xl:w-1/2 prose-h3:mb-0 h-auto'}>
+            <main className={'p-5 w-full md:w-2/3 prose-h3:mb-0 h-auto'}>
                 {children}
             </main>
         </div>
