680e7fcc206639ba6249199a4b9136679fed005d
Windows-Jovan <jovanus@yahoo.co.id>
2022-02-13 17:59:54 +0700

    Changing the font-file into woff2 variable font

--- components/layout.tsx
+++ components/layout.tsx
@@ -10,11 +10,7 @@ function Layout({children}: LayoutProps) {
         <>
             <Head>
                 <title>Covid-19 Information</title>
-                <link rel="preload" href="/fonts/PlusJakartaSans-Light.woff2" as="font" type="font/woff2" crossOrigin="anonymous"/>
-                <link rel="preload" href="/fonts/PlusJakartaSans-Regular.woff2" as="font" type="font/woff2" crossOrigin="anonymous"/>
-                <link rel="preload" href="/fonts/PlusJakartaSans-Medium.woff2" as="font" type="font/woff2" crossOrigin="anonymous"/>
-                <link rel="preload" href="/fonts/PlusJakartaSans-SemiBold.woff2" as="font" type="font/woff2" crossOrigin="anonymous"/>
-                <link rel="preload" href="/fonts/PlusJakartaSans-Bold.woff2" as="font" type="font/woff2" crossOrigin="anonymous"/>
+                <link rel="preload" href="/fonts/PlusJakartaSans.woff2" as="font" type="font/woff2" crossOrigin="anonymous"/>
             </Head>
             <div className={'prose prose-gray md:prose-lg xl:prose-xl flex justify-center bg-gray-50 max-w-none min-h-screen'}>
                 <main className={'p-5 w-full md:w-4/5 prose-h3:mb-0 h-auto'}>
