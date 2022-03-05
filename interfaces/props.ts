d887dfafcfe6462591ac261430017d77c5b6e2c9
jovan <jovanus@yahoo.co.id>
2022-03-05 13:39:24 +0700

    Altering the ProvinceData interface key for province detail

--- interfaces/props.ts
+++ interfaces/props.ts
@@ -10,14 +10,6 @@ export interface ProvinceProps {
     provinceData: ProvinceData[]
 }
 
-export interface ProvinceData {
-    provinsi: string;
-    kasus: number;
-    dirawat: number;
-    sembuh: number;
-    meninggal: number;
-}
-
 export interface PieChartData {
     label: string,
     value: number,
@@ -37,4 +29,42 @@ export interface DetailsProps {
 export interface LineChartProps {
     title: string,
     data: { date: number, value: number, secondValue?: number }[],
+}
+
+export interface ProvinceData {
+    provinsi: string;
+    kasus: number;
+    dirawat: number;
+    sembuh: number;
+    meninggal: number;
+    last_date: string;
+    jenis_kelamin: JenisKelamin;
+    kelompok_umur: KelompokUmur;
+    penambahan: Penambahan;
+    lokasi: Lokasi;
+}
+
+export interface JenisKelamin {
+    'laki-laki': number;
+    perempuan: number;
+}
+
+export interface KelompokUmur {
+    '0-5': number;
+    '6-18': number;
+    '19-30': number;
+    '31-45': number;
+    '46-59': number;
+    '≥ 60': number;
+}
+
+export interface Penambahan {
+    positif: number;
+    sembuh: number;
+    meninggal: number;
+}
+
+export interface Lokasi {
+    lon: number;
+    lat: number;
 }
